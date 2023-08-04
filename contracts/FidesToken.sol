521eeaee8595326c2ccddfb9e0c2a32b158a2bd0
rexjoseph <josephrex96@gmail.com>
2023-08-04 02:15:37 -0700

    3. Delegated transfers

--- contracts/FidesToken.sol
+++ contracts/FidesToken.sol
@@ -6,13 +6,22 @@ contract FidesToken {
   string public standard = 'Fides Token v1.0';
   uint256 public totalSupply;
 
+  // transfer event
   event Transfer(
     address indexed _from,
     address indexed _to,
     uint256 _value
   );
 
+  // approval event
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -24,7 +33,29 @@ contract FidesToken {
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
 
+    // emit transfer
     emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    // allowance
+    allowance[msg.sender][_spender] = _value;
+    // emit approval
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    allowance[_from][msg.sender] -= _value;
+
+    emit Transfer(_from, _to, _value);
     return true;
   }
 }
